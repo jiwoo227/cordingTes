ac3a1d742877fc5393ca311a7b1c4fd35c721599
최지우 <w2040@e-mirim.hs.kr>
2021-05-10 09:57:20 +0900

    펠린드롬

--- 문제07-1.py
+++ 문제07-1.py
@@ -5,25 +5,7 @@
 
 a = "neven"
 
-if a[0] == a[4]:
-    print("펠린드롬이 아니다.")
-elif a[1] != a[3]:
-    print("펠린드롬이 아니다.")
-else:
+for i in range(len(a)//2):
+    if a[i] != a[len(a)-1 -1]:
+        print("펠린드롬이 아니다.")
     print("펠린드롬이다.")
-
-#5글자인 경우
- #i값 0,i
-for i in range(2):
-    if a[i] != a[4-i]:
-        print("펠린드롬이 아니다. ")
-
-
-#7글자인경우
-for i in range(3):
-    if a[i] != a[6-i]:
-        print("펠린드롬이 아니다. ")
-
-else:
-    print("펠린드롬이다.")
-

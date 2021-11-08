01fb9acfe3c0f858ae93ce2fdce76c69308e1206
최지우 <w2040@e-mirim.hs.kr>
2021-11-08 09:52:25 +0900

    알고리즘 9번 문제

---
+++ 알고리즘 9.py
@@ -0,0 +1,3 @@
+students = ["김씨", "최씨", "이씨", "박씨", "정씨", "장씨"]
+for number, name in enumerate(students):
+    print("번호: {}, 성: {}".format(number, name))
--- 알고리즘7.py
+++ 알고리즘7.py
@@ -1,8 +1,8 @@
 arr = [1, 2, 2, 2, 3, 1, 1, 3, 2]
 
-새로운공간 = list()
+result = list()
 for x in arr:
     if x != 2:
-        새로운공간.append(x)
+       result.append(x)
 
-print(새로운공간)
+print(result)
